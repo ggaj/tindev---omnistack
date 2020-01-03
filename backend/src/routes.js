217ef26f011ad82c21d07b2c25eb8f83676529a2
Gildo Jr <gildojr@MacBook-Air-de-Gildo.local>
2020-01-02 22:45:26 -0400

    project omnistack-8

---
+++ backend/src/routes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+
+const routes = Router();
+
+const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
+
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
+
+module.exports = routes;
